d7fd2fd233f7301f3d03a6045b7863c78e31c7ae
Victor Herrera <hmvictor@users.noreply.github.com>
2016-03-13 23:20:04 -0600

    - Draft of SonarQube editor annotations.

--- src/main/java/qubexplorer/ui/IssuesTask.java
+++ src/main/java/qubexplorer/ui/IssuesTask.java
@@ -1,11 +1,25 @@
 package qubexplorer.ui;
 
+import java.io.File;
+import java.text.MessageFormat;
 import java.util.List;
+import javax.swing.JOptionPane;
+import org.openide.cookies.EditorCookie;
+import org.openide.cookies.LineCookie;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.text.Annotation;
+import org.openide.text.Line;
+import org.openide.util.Exceptions;
 import org.openide.windows.WindowManager;
 import qubexplorer.IssuesContainer;
+import qubexplorer.MvnModelInputException;
 import qubexplorer.RadarIssue;
 import qubexplorer.filter.IssueFilter;
 import qubexplorer.server.SonarQube;
+import qubexplorer.ui.editorannotations.InfoAnnotation;
 import qubexplorer.ui.task.Task;
 
 /**
@@ -34,6 +48,34 @@ public class IssuesTask extends Task<List<RadarIssue>>{
         sonarTopComponent.requestVisible();
         sonarTopComponent.setProjectContext(getProjectContext());
         sonarTopComponent.showIssues(filters, result.toArray(new RadarIssue[0]));
+        for (RadarIssue radarIssue : result) {
+            try {
+                if(radarIssue.line() == null) {
+                    continue;
+                }
+                IssueLocation issueLocation = new IssueLocation(radarIssue.componentKey(), radarIssue.line());
+                File file = issueLocation.getFile(getProjectContext().getProject(), getProjectContext().getConfiguration());
+                FileObject fileObject = FileUtil.toFileObject(file);
+                if (fileObject == null) {
+                    String messageTitle = org.openide.util.NbBundle.getMessage(SonarIssuesTopComponent.class, "SonarIssuesTopComponent.unexistentFile.title");
+                    String message = MessageFormat.format(org.openide.util.NbBundle.getMessage(SonarIssuesTopComponent.class, "SonarIssuesTopComponent.unexistentFile.text"), file.getPath());
+                    JOptionPane.showMessageDialog(WindowManager.getDefault().getMainWindow(), message, messageTitle, JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                DataObject dataObject = DataObject.find(fileObject);
+                LineCookie lineCookie = (LineCookie)dataObject.getLookup().lookup(LineCookie.class);
+                Line.Set lineSet = lineCookie.getLineSet();
+                int index=Math.min(radarIssue.line(), lineSet.getLines().size())-1;
+                final Line line = lineSet.getOriginal(index);
+                final Annotation ann = new InfoAnnotation();
+                if(line != null) {
+                    ann.attach(line);
+                }
+            } catch (MvnModelInputException | DataObjectNotFoundException ex) {
+                ;
+            }
+        }
+        
     }
     
 }
